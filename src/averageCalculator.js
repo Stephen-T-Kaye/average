da1d5326b137e28f9396951bd456f3f7d6cbf43e
Stephen Kaye <stephen.t.kaye@gmail.com>
2024-02-04 20:53:26 +0000

    Add averagCalculator implementation

--- src/averageCalculator.js
+++ src/averageCalculator.js
@@ -1,7 +1,19 @@
 const averageCalculator = function createAverageCalculator() {
+  let currentAverage = 0;
+  let sampleSize = 0;
   return {
-    addValue: function addValue(newValue) {},
-    getAverage: function getAverage() {},
+    addValue: function addValue(newValue) {
+      if (typeof newValue !== 'number') {
+        throw new Error('not a number');
+      }
+
+      sampleSize += 1;
+      currentAverage =
+        currentAverage + (newValue - currentAverage) / sampleSize;
+    },
+    getAverage: function getAverage() {
+      return currentAverage;
+    },
   };
 };
 
