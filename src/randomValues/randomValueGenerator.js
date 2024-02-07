771f5424159987fbc64e58fc3f97f518e7e8bb8f
Stephen Kaye <stephen.t.kaye@gmail.com>
2024-02-07 00:15:33 +0000

    Fix code quality issue missue of promise in timeout

--- src/randomValues/randomValueGenerator.js
+++ src/randomValues/randomValueGenerator.js
@@ -24,26 +24,28 @@ const createRandomValueGenerator = function createRandomValueGenerator(
   let stopped = false;
 
   const axiosInstance = axios.create({timeout: 1000});
-  const callApi = async () => {
-    try {
-      const resp = await axiosInstance.get(url);
-      if (resp.data[0].status === 'success') {
-        onValueGenerated(resp.data[0].random);
-      } else if (resp.data[0].code != 5) {
-        throw new Error('API error');
-      }
+  const callApi = () => {
+    axiosInstance
+        .get(url)
+        .then((resp) => {
+          if (resp.data[0].status === 'success') {
+            onValueGenerated(resp.data[0].random);
+          } else if (resp.data[0].code != 5) {
+            throw new Error('API error');
+          }
 
-      if (!stopped) {
-        timeoutHandle = setTimeout(callApi, interval);
-      }
-    } catch (err) {
-      if (onError) {
-        onError(new Error('API Error'));
-      }
-      if (err.code === 'ECONNABORTED' && !stopped) {
-        timeoutHandle = setTimeout(callApi, interval);
-      }
-    }
+          if (!stopped) {
+            timeoutHandle = setTimeout(callApi, interval);
+          }
+        })
+        .catch((err) => {
+          if (onError) {
+            onError(new Error('API Error'));
+          }
+          if (err.code === 'ECONNABORTED' && !stopped) {
+            timeoutHandle = setTimeout(callApi, interval);
+          }
+        });
   };
 
   setTimeout(callApi, interval);
