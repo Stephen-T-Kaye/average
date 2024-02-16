572ce4e5d132adcf7c809e9921cd0fa271c49fb3
Stephen Kaye <stephen.t.kaye@gmail.com>
2024-02-16 13:58:16 +0000

    Add frequency function

--- src/randomValues/averageCalculator.js
+++ src/randomValues/averageCalculator.js
@@ -1,6 +1,7 @@
 const createAverageCalculator = function createAverageCalculator() {
   let currentAverage = 0;
   let sampleSize = 0;
+  const frequencies = {};
   return {
     addValue: function addValue(newValue) {
       if (typeof newValue !== 'number') {
@@ -10,9 +11,16 @@ const createAverageCalculator = function createAverageCalculator() {
       sampleSize += 1;
       currentAverage =
         currentAverage + (newValue - currentAverage) / sampleSize;
+      if (!frequencies.hasOwnProperty(newValue)) {
+        frequencies[newValue] = 0;
+      }
+      frequencies[newValue]++;
     },
     getAverage: function getAverage() {
       return currentAverage;
+    },
+    getFrequency: function getFrequency(num) {
+      return frequencies[num] || 0;
     },
   };
 };
